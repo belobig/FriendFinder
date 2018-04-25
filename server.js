ebf2c23e7b8215fc70187a9f958838f2802397e2
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-04-25 09:46:06 -0600

    Another attempt to commit with stuff in files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Need to put something here to connect to htmlRoutes.js and apiRoutes.js
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
