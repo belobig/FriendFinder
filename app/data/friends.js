9ede3229d3826aaf4633e922f46695207ef69de9
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-04-26 18:34:02 -0600

    Mostly done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,35 @@
 var friendsArray = [{
-	name: "The Million Dollar Man Ted Dibiase",
-	photo: "https://s-media-cache-ak0.pinimg.com/originals/a9/09/b4/a909b465a163e1c0b895b6e8843d99bc.jpg",
+	name: "This dude",
+	photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT85pRWdh12j_oyX7b0Ya7HotUZbX3kRTF00ZSruDXl2sUllkvzmg",
 	scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
 }, {
-	name: "The Honkey Tonk Man",
-	photo: "http://www.writeups.org/wp-content/uploads/Honky-Tonk-Man-wrestler.jpg",
+	name: "Old guy with a tude",
+	photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsCflj0E-14cg2gJP8j3_SzMmQ-VfVMnG14bxfo1hmz_XrZx5bkg",
 	scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 },
 {
-	name: "Mr. Perfect",
-	photo: "http://iamjekker.com/wp-content/uploads/2015/06/9156859_orig.jpg",
+	name: "The Perfect Woman",
+	photo: "https://scstylecaster.files.wordpress.com/2013/12/perfect-woman-cropped.jpg",
 	scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 },
 {
-	name: "The Macho Man Randy Savage",
-	photo: "https://s-media-cache-ak0.pinimg.com/236x/83/12/7a/83127a08e032b7ecf0ba28ce7c27ba81.jpg",
+	name: "This guy",
+	photo: "http://ofad.org/files/imagecache/daily_picture/daily-photo/ofad-photo-of-3287_0.jpg",
 	scores: [4, 1, 1, 2, 5, 4, 5, 2, 4, 2]
 },
 {
-	name: "Jake the Snake Roberts",
-	photo: "http://splitsider.awlnetwork.com/wp-content/uploads/sites/2/2016/09/jake-the-snake-roberts.jpg",
+	name: "A real winner",
+	photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK4DcRDCT89h_srab-PD7XaDniUp7ksi4bgnT7jh2Uw5txr32g",
 	scores: [3, 3, 3, 1, 1, 5, 5, 4, 5, 2]
 },
 {
-	name: "Andre the Giant",
-	photo: "https://vignette2.wikia.nocookie.net/supergreatfrien/images/4/40/Andre.png/revision/latest?cb=20140709032847",
+	name: "Whoever this is",
+	photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4aLGi0uutzTvjhLUZAfApH42idpPB8goru9zM68gWbrV95VS6aw",
 	scores: [2, 2, 3, 4, 5, 2, 1, 4, 4, 4]
 },
 {
-	name: "Mantaur",
-	photo: "https://ringthedamnbell.files.wordpress.com/2015/11/mantas.jpg",
+	name: "You lost the genetic lottery",
+	photo: "http://www.bajiroo.com/wp-content/uploads/2013/03/daily-random-funny-weird-ugly-strange-people-girls-women-world-photos-pictures-images-pics-lol-32.jpg",
 	scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 }
 ];
